6b3dc69cd88e09a337c8ccf3f6c35891be9ae429
erichli1 <ehli@college.harvard.edu>
2022-08-24 22:24:47 -0400

    Add text input feature

---
+++ src/TextInput.js
@@ -0,0 +1,34 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+function TextInput(props) {
+  return (
+    <div>
+      <br/>
+      <h3>Total and people</h3>
+      <Form onSubmit={props.handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Text input below:</Form.Label>
+          <br/>
+          <Form.Text className="text-muted">
+            <p>
+              Total as number // People as list // Itemized costs with costs then names (semicolon separated)
+            </p>
+            <p>
+              123.45 // Jack Joe Jane // 50 Jack Joe; 40 Joe Jane; 30 Jane Jack
+            </p>
+          </Form.Text>
+          <Form.Control required as="textarea" type="text" placeholder="TOTAL // PEOPLE // ITEMIZED COSTS" name="text-input" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit text input
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default TextInput;
