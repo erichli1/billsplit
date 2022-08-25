8ba761484b10d3a63bd8a2aa4dc4a58d0a61a064
erichli1 <ehli@college.harvard.edu>
2022-08-24 22:57:51 -0400

    Clean up add item button UX

--- src/ItemizedSpending.js
+++ src/ItemizedSpending.js
@@ -8,12 +8,13 @@ function ItemizedSpending(props) {
   return (
     <div>
       <h3>Itemized spending</h3>
+      <p>Please click "Add item" to add a new line item</p>
       <Form onSubmit={props.handleSubmit}>
         <Container>
           {props.itemList}
         </Container>
         <Button variant="secondary" onClick={props.addItem}>Add item</Button>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" style={{marginLeft: 20 }}>
           Show me the split!
         </Button>
       </Form>
